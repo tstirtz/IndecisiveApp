75b938044feda9e5fc4dc59a15eea2fff953bb61
tstirtz <tyler.stirtz@gmail.com>
2021-09-24 13:53:13 -0400

    add controllers for entities

---
+++ Controllers/TagsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IndecisiveApp.Data;
+using IndecisiveApp.Models;
+
+namespace IndecisiveApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly IndecisiveContext _context;
+
+        public TagsController(IndecisiveContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tags
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tag>>> GetTags()
+        {
+            return await _context.Tags.ToListAsync();
+        }
+
+        // GET: api/Tags/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tag>> GetTag(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return tag;
+        }
+
+        // PUT: api/Tags/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTag(int id, Tag tag)
+        {
+            if (id != tag.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tags
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tag>> PostTag(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTag), new { id = tag.ID }, tag);
+        }
+
+        // DELETE: api/Tags/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTag(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Tags.Any(e => e.ID == id);
+        }
+    }
+}
