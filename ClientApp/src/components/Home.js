054e9f72bf2fdd9632a0f002dcda0d4a590980a9
tstirtz <tyler.stirtz@gmail.com>
2021-09-24 16:08:42 -0400

    add axios, add method to fetch categories and items

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
+import {Category} from './Category';
+import axios from 'axios';
 
 export class Home extends Component {
-  static displayName = Home.name;
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      isFetching: false
+    }
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  fetchCategories() {
+      this.setState({...this.state, isFetching: true});
+      axios.get("https://localhost:44393/api/categories").then(response => {
+        this.setState({categories: response.data, isFetching: false});
+      })
+      .catch(e => {
+      console.log(e);
+      this.setState({...this.state, isFetching: false});
+    });
+  }
+  
+  componentDidMount() {
+    this.fetchCategories()
+  }
 
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        {
+          this.state.categories.map((category, index) => <Category key={index} category={category}></Category>)
+        }
       </div>
     );
   }
